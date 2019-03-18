c1b618966e398d91bb91c298c17899c50aff0355
Peter Ramos <peterdramos02@gmail.com>
2019-03-18 15:25:11 -0400

    add passport and jwt to index.js and mount routers

--- index.js
+++ index.js
@@ -3,24 +3,64 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+const passport = require('passport');
 
 const { PORT, CLIENT_ORIGIN } = require('./config');
 const { dbConnect } = require('./db-mongoose');
-// const {dbConnect} = require('./db-knex');
+const localStrategy = require('./passport/local');
+const jwtStrategy = require('./passport/jwt');
+
+// Import routers
+const usersRouter = require('./routes/users');
+const authRouter = require('./routes/auth');
+
 
 const app = express();
 
+// Log all requests
 app.use(
   morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
     skip: (req, res) => process.env.NODE_ENV === 'test'
   })
 );
 
+// Set up CORS
 app.use(
   cors({
     origin: CLIENT_ORIGIN
   })
 );
+
+// Parse request body
+app.use(express.json());
+
+// Utilize the given strategy
+passport.use(localStrategy);
+passport.use(jwtStrategy);
+
+// Protext endpoints using JWT Strategy
+const jwtAuth = passport.authenticate('jwt', { session: false, failWithError: true });
+
+// Mount routers
+app.use('/api/users', jwtAuth, usersRouter);
+app.use('api', authRouter);
+
+// Custom 404 Not Found route handler
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 400;
+  next(err);
+});
+
+// Custom Error Handler
+app.use((err, req, res, next) => {
+  if (err.status) {
+    const errBody = {...err, message: err.message};
+    res.status(err.status).json(errBody);
+  } else {
+    res.status(500).json({ message: 'Internal Server Error'});
+  }
+});
 
 function runServer(port = PORT) {
   const server = app
