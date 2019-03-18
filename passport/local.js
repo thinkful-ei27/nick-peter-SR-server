8c2111852f2ff56b5335b12f969738dca4efa5e6
Peter Ramos <peterdramos02@gmail.com>
2019-03-18 14:44:38 -0400

    add passport local strategy

---
+++ passport/local.js
@@ -0,0 +1,40 @@
+const { Strategy: LocalStrategy } = require('passport-local');
+
+const User = require('../models/user');
+
+// ======= Define and create localStrategy ========
+const localStrategy = new LocalStrategy((username, password, done) => {
+  let user;
+  User.findOne({ username })
+    .then(results => {
+      user = results;
+      if (!user) {
+        return Promise.reject({
+          reason: 'LoginError',
+          message: 'Incorrect username',
+          location: 'username',
+          status: 401
+        });
+      }
+      return user.validatePassword(password);
+    })
+    .then(isValid => {
+      if (!isValid) {
+        return Promise.reject({
+          reason: 'LoginError',
+          message: 'Incorrect password',
+          location: 'password',
+          status: 401
+        });
+      }
+      return done(null, user);
+    })
+    .catch(err => {
+      if (err.reason === 'LoginError') {
+        return done(null, false);
+      }
+      return done(err);
+    });
+});
+
+module.exports = localStrategy;
