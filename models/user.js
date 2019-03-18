b924f642bf09591c764e2d93fff41dd9bf5a2e78
Peter Ramos <peterdramos02@gmail.com>
2019-03-18 14:52:56 -0400

    add bcrypt dep and use to hash and compare passwords

--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 'user strict';
 
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const schema = new mongoose.Schema({
   username: { type: String, required: true, unique: true},
@@ -16,5 +17,15 @@ schema.set('toJSON', {
     delete result.password;
   }
 });
+
+// Note: Use `function` keyword instead of `=>` to allow setting of `this`
+schema.methods.validatePassword = function (pwd) {
+  const currentUser = this;
+  return bcrypt.compare(pwd, currentUser.password);
+};
+
+schema.statics.hashPassword = function (pwd) {
+  return bcrypt.hash(pwd, 10);
+};
 
 module.exports = mongoose.model('User', schema);
