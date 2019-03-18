e2012124246c6f6d01fc3260034da3a1160d5876
Peter Ramos <peterdramos02@gmail.com>
2019-03-18 14:36:54 -0400

    add jwt strategy

--- passport/jwt.js
+++ passport/jwt.js
@@ -1,3 +1,15 @@
 const { Strategy: JwtStrategy, ExtractJwt} = require('passport-jwt');
 
 const { JWT_SECRET } = require('../config');
+
+const options = {
+  secretOrKey: JWT_SECRET,
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken('Bearer'),
+  algorithms: ['HSA256']
+};
+
+const jwtStrategy = new JwtStrategy(options, (payload, done) => {
+  done(null, payload.user);
+});
+
+module.exports = jwtStrategy;
