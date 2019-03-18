ec29e602d1f830c7d668f38b3d44763b594692f5
Peter Ramos <peterdramos02@gmail.com>
2019-03-18 15:13:38 -0400

    add auth routes - isse jwt for login and refresh

--- config.js
+++ config.js
@@ -9,10 +9,6 @@ module.exports = {
   TEST_DATABASE_URL:
         process.env.TEST_DATABASE_URL ||
         'mongodb://localhost/thinkful-backend-test',
-  JWT_SECRET: process.env.JWT_SECRET
-  // DATABASE_URL:
-  //     process.env.DATABASE_URL || 'postgres://localhost/thinkful-backend',
-  // TEST_DATABASE_URL:
-  //     process.env.TEST_DATABASE_URL ||
-  //     'postgres://localhost/thinkful-backend-test'
+  JWT_SECRET: process.env.JWT_SECRET,
+  JWT_EXPIRY: process.env.JWT_EXPIRY || '7d'
 };
---
+++ routes/auth.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const router = express.Router();
+
+const { JWT_SECRET, JWT_EXPIRY } = require('../config');
+
+const localAuth = passport.authenticate('local', { session: false, failWithError: true });
+
+router.post('/login', (req, res, next) => {
+  const authToken = createAuthToken(req.user);
+  res.json({ authToken });  
+});
+
+const jwtAuth = passport.authenticate('jwt', { session: false, failWithError: true });
+
+router.post('/refresh', (req, res, next) => {
+  const authToken = createAuthToken(req.user);
+  res.json({ authToken });
+});
+
+function createAuthToken(user) {
+  return jwt.sign({ user }, JWT_SECRET, {
+    subject: user.username,
+    expiresIn: JWT_EXPIRY
+  });
+}
+
+module.exports = router;
