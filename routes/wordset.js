141eb0dfb15a675af93b32836313a7f72cd5aeff
nicknotj <nicknotjustin@gmail.com>
2019-03-20 16:50:04 -0500

    Both the correct and incorrect paths for the post / route work, although they will eventually break with high enough m strengths. Yeah.

--- routes/wordset.js
+++ routes/wordset.js
@@ -28,8 +28,7 @@ router.get('/', jwtAuth, (req, res, next) => {
       return User.find(options)
         .slice('wordSet', [index, 1])
         .then(result => {
-          console.log(result[0].wordSet[0].portuguese);
-          res.json(result);
+          res.json(result[0].wordSet[0].portuguese);
         });
     })
     .catch(err => next(err));
@@ -38,69 +37,64 @@ router.get('/', jwtAuth, (req, res, next) => {
 router.post('/', jwtAuth, (req, res, next) => {
   let userId = req.user.id;
   let { answer } = req.body;
-  answer = answer.toLowerCase();
-  if(!answer){
+  let userAnswer = answer.toLowerCase();
+  if(!userAnswer){
     return res.status(400).json({
       code: 400,
-      reason: 'ValidationError', //???
+      reason: 'ValidationError',
       message: 'Please submit an answer',
       location: 'answer' //check with client
     });
   }
-  return User.find({ _id: userId }, { head: 1 })
+  return User.find({ _id: userId })
     .then(([result]) => {
-      res.json(result.head);
-      const index = result.head;
-      const field = 'wordSet.' + index;
-      const projection = { '_id': 0};
-      projection[field] = 1;
-    })  
-  //compare answer to head's answer/english value
-  //(True) new memoryStrength = oldValue * 2
-  //new head number equals old head's next
-  //using the head (number) as the index, find the word's next
-  //set head to that next value (index of new head)
-  //while loop (n<oldValue, next !== null) Traverse using temp node
-  //set tempNodes.next equal to answered word
-  //set answered word's next equal to original tempNodes.next
-
-
-  //this is the current head index
-  // let head = result[0].head;
-  // //the head (word wise)
-  // let word = result[0].wordSet[head];
-  // //number that will indicate how many spaces the word will move (if correct)
-  // let oldValue = word.memoryStrength;
-  // //number that will be turned into the new head indicator
-  // let next = result[0].wordSet[head].next;
-  // //this will be the updated memoryStrength
-  // let newValue;
-  // if(answer === word.english){
-  //   newValue = oldValue * 2;
-  //   return User.findOneAndUpdate({_id: userId}, {$set: {"head": next}})
-  //   .then(() => res.json(`Correct!`));
-  // } else {
-  //   newValue = 1;
-  //   return User.findOneAndUpdate({_id: userId}, {$set: {"head": next}})
-  //   .then(() => res.json(`Incorrect. The correct answer is ${word.english}`));
-  // }
-      
-
-
-  // if(answer === wordSet[0].English){
-  //   let oldValue = newValue;
-  //   newValue = newValue * 2;
-  //   return User.findOneAndUpdate({ _id: userId}, {$set: {"wordSet.0.M": newValue}})
-  //   .then(() => res.json('Correct!'));
-  // } else {
-  //   newValue = newValue - 1;
-  //   if(newValue < 0){
-  //     newValue = 0;
-  //   }
-  //   let response = `Incorrect. The correct answer is ${wordSet[0].English}`;
-  //   return User.findOneAndUpdate({ _id: userId}, {$set: {"wordSet.0.M": newValue}})
-  //   .then(() => res.json(response));
-  // }
+      //this is a number pointing to head's index
+      const oldHead = result.head;
+      //gets the node/card at head
+      let words = result.wordSet;
+      const headNode = result.wordSet[oldHead];
+      const trueAnswer = headNode.english;
+      if(userAnswer === trueAnswer){
+        const mStrength = headNode.memoryStrength * 2;
+        const newHead = headNode.next;
+        let prevNode = newHead;
+        let tempNode = newHead;
+        for(let x = 0; x < mStrength; x++){
+          //We will need a base case (tempNode === null?)
+          prevNode = tempNode;
+          tempNode = words[tempNode].next;
+          console.log(`prevNode is ${prevNode}`);
+          console.log(`tempNode is ${tempNode}`);
+        }
+        words[prevNode].next = oldHead;
+        words[oldHead].next = tempNode;
+        words[oldHead].memoryStrength = mStrength;
+        console.log(words);
+        //HOT SPICY MANGO POLICY
+        const update = {};
+        update["wordSet"] = words;
+        update["head"] = newHead;
+        return User.findOneAndUpdate({ _id: userId}, update)
+        .then(() => {
+          res.json('Correct!')
+        })
+      } else {
+        words[oldHead].memoryStrength = 1;
+        const newHead = headNode.next;
+        const oldNext = words[newHead].next;
+        words[newHead].next = oldHead;
+        words[oldHead].next = oldNext;
+        console.log(words[oldHead]);
+        //HOT SPICY MANGO POLICY
+        const update = {};
+        update["wordSet"] = words;
+        update["head"] = newHead;
+        return User.findOneAndUpdate({ _id: userId}, update)
+        .then(() => {
+          res.json(`Incorrect. The correct answer is ${trueAnswer}`)
+        })
+      }
+  })  
     .catch(err => {
       next(err);
     });
